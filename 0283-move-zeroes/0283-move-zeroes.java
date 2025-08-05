3f1f48a735623ab903ab8c2debf0ede795d464d7
Dhanushgowdamv <150064120+Dhanushgowdamv@users.noreply.github.com>
2025-08-05 11:23:28 +0530

    Time: 1 ms (99.71%), Space: 46.2 MB (49.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,14 @@
 class Solution {// tc is o(n) and sc is O(1)
     public void moveZeroes(int[] nums) {
-        int slow = 0;
-        for(int fast=0 ; fast < nums.length;fast++){
-            if(nums[fast] !=0){
-                int temp = nums[fast];
-                nums[fast] = nums[slow];
-                nums[slow] = temp;
-
-                slow++;
+        int insertpos = 0;
+        for(int num:nums){
+            if(num!=0){
+                nums[insertpos++] = num;
             }
-            
         }
+        while (insertpos < nums.length) {
+            nums[insertpos++] = 0;
+        }
+
     }
 }
